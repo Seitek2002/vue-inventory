60958627d3983e9dfadb391c0f3b6278bad8868a
Seitek2002 <seitek_2002@bk.ru>
2022-12-17 00:40:00 +0600

    table create

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,21 @@
   <div class="wrapper">
     <div class="wrapper__content">
       <Sidebar />
+      <Table />
     </div>
   </div>
 </template>
 
 <script>
 import Sidebar from "@/components/Sidebar.vue";
+import Table from "@/components/Table.vue";
 export default {
-  components: { Sidebar },
-}
+  components: { Sidebar, Table },
+};
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap");
 .wrapper {
   background: #1e1e1e;
   padding: 32px;
@@ -22,6 +24,12 @@ export default {
   height: 100vh;
   box-sizing: border-box;
   font-family: "Inter", sans-serif;
+
+  &__content {
+    display: flex;
+    justify-content: center;
+    gap: 24px;
+  }
 }
 
 body {
---
+++ src/components/Table.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="table">
+    <div
+      class="table__row"
+      v-for="(row, rowIndex) in Array(5)"
+      :key="rowIndex"
+    >
+      <div
+        class="table__cell"
+        v-for="(item, colIndex) in Array(5)"
+        :key="colIndex"
+      >
+        <div v-if="item" class="table__item">
+          <img src="" alt="" />
+          <div class="table__item-counter">{{ item.counter }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.table {
+  background: #262626;
+  border: 1px solid #4d4d4d;
+  border-radius: 12px;
+  width: 525px;
+  height: 500px;
+  overflow: hidden;
+
+  &__row {
+    display: flex;
+  }
+
+  &__cell {
+    width: 103px;
+    height: 98px;
+    border: 1px solid #4d4d4d;
+
+    img {
+      width: 54px;
+      height: 54px;
+    }
+  }
+
+  &__item {
+    position: relative;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: 0.3s;
+
+    &-counter {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      color: #ffffff;
+      opacity: 0.4;
+      font-size: 10px;
+      background: #262626;
+      border: 1px solid #4d4d4d;
+      border-radius: 6px 0px 0px 0px;
+      width: 16px;
+      height: 16px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+}
+</style>
