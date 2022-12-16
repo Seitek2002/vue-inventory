c2ed73c153b0d3b1e208571ca8087b54a4569a56
Seitek2002 <seitek_2002@bk.ru>
2022-12-17 00:34:48 +0600

    some styles on wrapper

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,17 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+.wrapper {
+  background: #1e1e1e;
+  padding: 32px;
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  font-family: "Inter", sans-serif;
+}
 
+body {
+  margin: 0;
+}
 </style>
